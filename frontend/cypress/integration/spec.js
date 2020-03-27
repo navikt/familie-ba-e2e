7e2975180f4b8a8b8df506d3efbd82878ceaf178
Henning Håkonsen <henninghaakonsen@me.com>
2020-03-27 13:48:22 +0100

    Begynt på implementering av cypress tester.

--- frontend/cypress/integration/spec.js
+++ frontend/cypress/integration/spec.js
@@ -1,5 +1,8 @@
 describe("Kjør enkel verdikjedetest på frontend", () => {
   it("Vis app og lag fagsak", () => {
     cy.visit("http://localhost:8000/");
+    cy.get("input[name=username]").type("123456789");
+    cy.get("button").click();
+    cy.wait(1000);
   });
 });
