e68999e433da183c0bf7e509dbf8e5c78c4e87ae
Henning Håkonsen <henninghaakonsen@me.com>
2020-03-28 12:40:53 +0100

    Debug uten detatch mode.

--- e2e/e2e.sh
+++ e2e/e2e.sh
@@ -4,11 +4,10 @@ export COMPOSE_IGNORE_ORPHANS=true
 
 docker-compose pull
 docker-compose build
-docker-compose up -d --force-recreate
+docker-compose up
 
 while [[ $(curl -s -X GET "http://host.docker.internal:8089/internal/health") == "" ]]; do
     echo "venter på oppstart av mottak / sak / integrasjoner"
-    docker ps
     sleep 1
 done
 
