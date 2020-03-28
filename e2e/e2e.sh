fe9c5c75d3b4917464d172d5d52767531f11682a
Henning Håkonsen <henninghaakonsen@me.com>
2020-03-28 11:54:59 +0100

    Fjerner venting på vtp i e2e script.

--- e2e/e2e.sh
+++ e2e/e2e.sh
@@ -6,15 +6,8 @@ docker-compose pull
 docker-compose build
 docker-compose up -d --force-recreate
 
-
-while [[ $(curl -s -X GET "http://localhost:8060/rest/isso/isAlive.jsp") == "" ]]; do
-    echo "venter på oppstart av VTP"
-    sleep 1
-done
-
-
 while [[ $(curl -s -X GET "http://localhost:8089/internal/health") == "" ]]; do
-    echo "venter på oppstart av mottak / sak / oppslag"
+    echo "venter på oppstart av mottak / sak / integrasjoner"
     sleep 1
 done
 
