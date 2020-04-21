93025445887d0fb5c5ddba3b5f7286f9652a390b
Henning Håkonsen <henninghaakonsen@me.com>
2020-04-21 20:05:15 +0200

    Prøver network mode.

--- e2e/e2e.sh
+++ e2e/e2e.sh
@@ -4,7 +4,7 @@ export COMPOSE_IGNORE_ORPHANS=true
 
 docker-compose pull
 docker-compose build
-<<docker-compose up --force-recreate
+docker-compose up
 
 while [[ $(curl -s -X GET "http://host.docker.internal:8089/internal/health") == "" ]]; do
     echo "venter på oppstart av mottak / sak / integrasjoner"
