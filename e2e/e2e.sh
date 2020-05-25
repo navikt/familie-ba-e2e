f0ed25099633a532ae8c708df8c257980b39127b
Henning Håkonsen <henninghaakonsen@me.com>
2020-05-25 11:51:12 +0200

    Prøver å vente på localhost istedet for hostname.

--- e2e/e2e.sh
+++ e2e/e2e.sh
@@ -6,7 +6,7 @@ docker-compose pull
 docker-compose build
 docker-compose up -d --force-recreate
 
-while [[ $(curl -s -X GET "http://host.docker.internal:8089/internal/health") == "" ]]; do
+while [[ $(curl -s -X GET "http://localhost:8089/internal/health") == "" ]]; do
     echo "venter på oppstart av mottak / sak / integrasjoner"
     sleep 1
 done
