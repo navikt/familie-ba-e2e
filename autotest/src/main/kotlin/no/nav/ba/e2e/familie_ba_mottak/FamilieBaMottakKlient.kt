92ae17857a22506736281bce45873c94df910c53
Stig Helge Strøm <stig.helge.strom@nav.no>
2020-06-10 23:11:34 +0200

    enabler ba-mottak. Enkle tester for pdl hendelser

---
+++ autotest/src/main/kotlin/no/nav/ba/e2e/familie_ba_mottak/FamilieBaMottakKlient.kt
@@ -0,0 +1,50 @@
+package no.nav.ba.e2e.familie_ba_mottak
+
+import no.nav.familie.http.client.AbstractRestClient
+import no.nav.familie.kontrakter.felles.Ressurs
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Service
+import org.springframework.web.client.RestOperations
+import org.springframework.web.client.getForEntity
+import org.springframework.web.client.postForEntity
+import java.net.URI
+
+@Service
+class FamilieBaMottakKlient(
+        @Value("\${FAMILIE_BA_MOTTAK_API_URL}") private val baMottakUrl: String,
+        private val restOperations: RestOperations
+) : AbstractRestClient(restOperations, "familie-ba-mottak") {
+
+    fun dødsfall(identer: List<String>): ResponseEntity<String> {
+        val uri = URI.create("$baMottakUrl/internal/e2e/pdl/doedsfall")
+
+        return restOperations.postForEntity(uri, identer)
+    }
+
+    fun fødsel(identer: List<String>): ResponseEntity<String> {
+        val uri = URI.create("$baMottakUrl/internal/e2e/pdl/foedsel")
+
+        return restOperations.postForEntity(uri, identer)
+    }
+
+    fun postJournalhendelse(journalpostId: String): ResponseEntity<String> {
+        val uri = URI.create("$baMottakUrl/internal/e2e/journal/$journalpostId")
+
+        return restOperations.postForEntity(uri, "foo")
+    }
+
+    fun erHendelseMottatt(hendelseId: String, consumer: String): ResponseEntity<Boolean> {
+        return restOperations.getForEntity("$baMottakUrl/internal/e2e/hendelselogg/$hendelseId/$consumer")
+    }
+
+    fun hentTasker(key: String, value: String): ResponseEntity<List<Task>> {
+        return restOperations.getForEntity("$baMottakUrl/internal/e2e/task/$key/$value")
+    }
+
+    fun truncate(): ResponseEntity<Ressurs<String>> {
+        val uri = URI.create("$baMottakUrl/internal/e2e/truncate")
+
+        return restOperations.getForEntity(uri)
+    }
+}
--- autotest/src/test/kotlin/no/nav/ba/e2e/autotest/AutotestLeesahTests.kt
+++ autotest/src/test/kotlin/no/nav/ba/e2e/autotest/AutotestLeesahTests.kt
@@ -1,43 +1,75 @@
 package no.nav.ba.e2e.autotest
 
-import no.nav.person.pdl.leesah.Endringstype
-import no.nav.person.pdl.leesah.Personhendelse
-import no.nav.person.pdl.leesah.foedsel.Foedsel
-import org.apache.avro.generic.GenericRecordBuilder
-import org.junit.jupiter.api.Disabled
+import no.nav.ba.e2e.familie_ba_mottak.FamilieBaMottakKlient
+import org.assertj.core.api.Assertions.assertThat
+import org.awaitility.kotlin.await
+import org.awaitility.kotlin.withPollInterval
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.slf4j.MDC
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import java.time.Instant
+import java.time.Duration
+import java.util.*
+import java.util.concurrent.TimeUnit
 
-@SpringBootTest
-@Disabled
-class AutotestLeesahTests {
+@SpringBootTest(classes = [ApplicationConfig::class])
 
-    @Autowired
-    lateinit var kafkaService: ProducerService
+class AutotestLeesahTests(@Autowired
+                          private val mottakKlient: FamilieBaMottakKlient
+) {
+
+
+    @BeforeEach
+    fun init() {
+        mottakKlient.truncate()
+    }
 
     @Test
-    fun verifiserStandardFødselshendelse() {
-        val personhendelse = GenericRecordBuilder(Personhendelse.`SCHEMA$`)
-        personhendelse.set("hendelseId", "2")
-        val personidenter = ArrayList<String>()
-        personidenter.add("1234567890123")
-        personidenter.add("12345678901")
-        personhendelse.set("personidenter", personidenter)
-        personhendelse.set("master", "")
-        personhendelse.set("opprettet", 0L)
-        personhendelse.set("opplysningstype", "FOEDSEL_V1")
-        personhendelse.set("endringstype", Endringstype.OPPRETTET)
+    fun `skal sende dødsfallhendelse`() {
+        val dødsfallResponse = mottakKlient.dødsfall(listOf("12345678901", "1234567890123"))
+        assertThat(dødsfallResponse.statusCode.is2xxSuccessful).isTrue()
+        assertThat(dødsfallResponse.body).isNotNull()
 
-        val fødsel = GenericRecordBuilder(Foedsel.`SCHEMA$`)
-        fødsel.set("foedselsdato", (Instant.now().toEpochMilli() / (1000 * 3600 * 24)).toInt()) //Setter dagens dato på avroformat
-        personhendelse.set("foedsel", fødsel.build())
-
-        kafkaService.sendMessage(personhendelse.build())
+        //Sjekk om det er et innslag i hendelselogg på meldingen
+        val erHendelseMottatt = mottakKlient.erHendelseMottatt(dødsfallResponse.body!!, "PDL")
+        assertThat(erHendelseMottatt.statusCode.is2xxSuccessful).isTrue()
+        assertThat(erHendelseMottatt.body).isTrue()
+    }
 
 
-        // TODO kalle rest endepunkt i ba-sak og sjekke at vi har lagret behandlingen.
+    @Test
+    fun `skal sende fødselshendelse`() {
+        val callId = UUID.randomUUID().toString()
+        MDC.put("callId", callId)
+        val fødselsHendelseResponse = mottakKlient.fødsel(listOf("12345678901", "1234567890123"))
+        assertThat(fødselsHendelseResponse.statusCode.is2xxSuccessful).isTrue()
+        assertThat(fødselsHendelseResponse.body).isNotNull()
+
+        //Sjekk om det er et innslag i hendelselogg på meldingen
+        val erHendelseMottatt = mottakKlient.erHendelseMottatt(fødselsHendelseResponse.body!!, "PDL")
+        assertThat(erHendelseMottatt.statusCode.is2xxSuccessful).isTrue()
+        assertThat(erHendelseMottatt.body).isTrue()
+
+        sjekkOmTaskEksisterer("mottaFødselshendelse", callId)
+
+        await.atMost(120, TimeUnit.SECONDS)
+                .withPollInterval(Duration.ofSeconds(1))
+                .until {
+                    sjekkOmTaskEksisterer("sendTilSak", callId)
+                }
+    }
+
+    private fun sjekkOmTaskEksisterer(taskStepType: String, callId: String): Boolean {
+        val tasker = mottakKlient.hentTasker("callId", callId)
+        try {
+            assertThat(tasker.body)
+                    .hasSizeGreaterThan(0)
+                    .extracting("taskStepType").contains(taskStepType)
+        } catch (e: AssertionError) {
+            return false
+        }
+        return true
     }
 
 }
