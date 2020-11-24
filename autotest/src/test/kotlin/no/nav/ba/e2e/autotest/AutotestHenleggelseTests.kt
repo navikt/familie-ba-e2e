4cebfa19af7e2583b3c3358ccdc7fb00ad238d24
Marcus Gustafson <marcus.gustafson@computas.com>
2020-11-24 07:43:01 +0100

    Utvidedet testet

--- autotest/src/test/kotlin/no/nav/ba/e2e/autotest/AutotestHenleggelseTests.kt
+++ autotest/src/test/kotlin/no/nav/ba/e2e/autotest/AutotestHenleggelseTests.kt
@@ -9,30 +9,42 @@ import no.nav.ba.e2e.familie_ba_sak.FamilieBaSakKlient
 import no.nav.ba.e2e.familie_ba_sak.domene.*
 import no.nav.familie.kontrakter.felles.Ressurs
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions
+import org.awaitility.kotlin.await
+import org.awaitility.kotlin.withPollInterval
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import java.time.Duration
+import java.util.concurrent.TimeUnit
 
 class AutotestHenleggelseTests(
         @Autowired mottakKlient: FamilieBaMottakKlient,
         @Autowired baSakKlient: FamilieBaSakKlient) : AbstractMottakTest(mottakKlient, baSakKlient) {
 
     @Test
-    fun `Henlegg en behandling og hent forhåndsvising av sendt brev`() {
+    fun `Henlegg en behandling, hent forhåndsvising av brev, henlegg og opprett ny behandling`() {
         val søkersIdent = morPersonident
-        val aktivBehandling = opprettBhandlingOgRegistrerSøknad(søkersIdent)
+        val førsteBehandling = opprettBhandlingOgRegistrerSøknad(søkersIdent)
 
-        val responseForhandsvis = baSakKlient.forhaandsvisHenleggelseBrev(aktivBehandling!!.behandlingId,
-                                                               RestHenleggDocGen(mottakerIdent = morPersonident,
-                                                                                 brevmal = "HENLEGGE_TRUKKET_SØKNAD"))
+        val responseForhandsvis = baSakKlient.forhaandsvisHenleggelseBrev(førsteBehandling!!.behandlingId,
+                                                                          RestHenleggDocGen(mottakerIdent = morPersonident,
+                                                                                            brevmal = "HENLEGGE_TRUKKET_SØKNAD"))
         assertThat(responseForhandsvis?.status == Ressurs.Status.SUKSESS)
 
         // foerstesidegenerator er kke tilgjengelig derfor kan man ikke prøve med SØKNED_TRUKKET.
-        val responseHenlagdSøknad = baSakKlient.henleggSøknad(aktivBehandling.behandlingId,
-                                                  RestHenleggelse(årsak = "FEILAKTIG_OPPRETTET", begrunnelse = "Søknad trukket"))
-        //assertThat(responseHenlagdSøknad.data.status) OPPRETTET
-        assertThat(responseHenlagdSøknad?.data?.behandlinger?.first()?.aktiv == false )
-        assertThat(responseHenlagdSøknad?.data?.behandlinger?.first()?.samletResultat == BehandlingResultatType.HENLAGT_FEILAKTIG_OPPRETTET )
+        val responseHenlagdSøknad = baSakKlient.henleggSøknad(førsteBehandling.behandlingId,
+                                                              RestHenleggelse(årsak = "FEILAKTIG_OPPRETTET",
+                                                                              begrunnelse = "Søknad trukket"))
+
+        await.atMost(80, TimeUnit.SECONDS).withPollInterval(Duration.ofSeconds(1)).until {
+            val fagsak = baSakKlient.hentFagsak(responseHenlagdSøknad!!.data!!.id).data
+            fagsak?.status == FagsakStatus.AVSLUTTET
+        }
+
+        assertThat(responseHenlagdSøknad?.data?.behandlinger?.first()?.aktiv == false)
+        assertThat(responseHenlagdSøknad?.data?.behandlinger?.first()?.samletResultat == BehandlingResultatType.HENLAGT_FEILAKTIG_OPPRETTET)
+
+        val andreBehandling = opprettBhandlingOgRegistrerSøknad(søkersIdent)
+        assertThat(andreBehandling?.aktiv == true)
     }
 
     private fun opprettBhandlingOgRegistrerSøknad(søkersIdent: String): RestBehandling? {
