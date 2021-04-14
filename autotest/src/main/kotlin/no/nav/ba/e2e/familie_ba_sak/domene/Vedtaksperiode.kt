d7357724031914050c2cf56676bfab4e529f3a66
Øyvind Hagen <oyvind.hagen@nav.no>
2021-04-14 09:52:41 +0200

    Konfigurerer jackson til å håndtere subclasser av Vedtaksperiode. Setter fom og tom på vedtaksbegrunnelse i samsvar med en av vedtaksperiodene for å hindre at de filtreres vekk i ba-sak.

--- autotest/src/main/kotlin/no/nav/ba/e2e/familie_ba_sak/domene/Vedtaksperiode.kt
+++ autotest/src/main/kotlin/no/nav/ba/e2e/familie_ba_sak/domene/Vedtaksperiode.kt
@@ -1,7 +1,15 @@
 package no.nav.ba.e2e.familie_ba_sak.domene
 
+import com.fasterxml.jackson.annotation.JsonSubTypes
+import com.fasterxml.jackson.annotation.JsonTypeInfo
 import java.time.LocalDate
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "vedtaksperiodetype")
+@JsonSubTypes(*arrayOf(
+        JsonSubTypes.Type(value = Utbetalingsperiode::class, name = "UTBETALING"),
+        JsonSubTypes.Type(value = Avslagsperiode::class, name = "AVSLAG"),
+        JsonSubTypes.Type(value = Opphørsperiode::class, name = "OPPHØR")
+))
 abstract class Vedtaksperiode(
         open val periodeFom: LocalDate?,
         open val periodeTom: LocalDate?,
