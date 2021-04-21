a05387ffd88e7ebf32ed571558a211d604a2bf29
Marcus Gustafson <marcus.gustafson@computas.com>
2021-04-21 15:14:16 +0200

    Oppdateringer etter QA

--- autotest/src/test/kotlin/no/nav/ba/e2e/autotest/AutotestBehandlingOmgjøringTests.kt
+++ autotest/src/test/kotlin/no/nav/ba/e2e/autotest/AutotestBehandlingOmgjøringTests.kt
@@ -1,32 +1,43 @@
 package no.nav.ba.e2e.autotest
 
-import no.nav.ba.e2e.commons.*
-import no.nav.ba.e2e.familie_ba_mottak.FamilieBaMottakKlient
+import no.nav.ba.e2e.commons.kjørStegprosessForFGB
 import no.nav.ba.e2e.familie_ba_sak.FamilieBaSakKlient
-import no.nav.ba.e2e.familie_ba_sak.domene.*
+import no.nav.ba.e2e.familie_ba_sak.domene.BehandlingStatus
+import no.nav.ba.e2e.familie_ba_sak.domene.BehandlingÅrsak
+import no.nav.ba.e2e.familie_ba_sak.domene.StegType
 import no.nav.ba.e2e.mockserver.MockserverKlient
 import no.nav.ba.e2e.mockserver.domene.RestScenario
 import no.nav.ba.e2e.mockserver.domene.RestScenarioPerson
-import no.nav.familie.kontrakter.felles.Ressurs
-import org.assertj.core.api.Assertions.assertThat
 import org.awaitility.kotlin.await
 import org.awaitility.kotlin.withPollInterval
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
 import java.time.Duration
 import java.time.LocalDate
 import java.util.concurrent.TimeUnit
 
-class AutotestBehandlingOmgjøringTests(
-        @Autowired mottakKlient: FamilieBaMottakKlient,
-        @Autowired baSakKlient: FamilieBaSakKlient,
-        @Autowired mockserverKlient: MockserverKlient) : AbstractMottakTest(mottakKlient, baSakKlient, mockserverKlient) {
+@SpringBootTest(classes = [ApplicationConfig::class])
+class TestBehandlingOmgjøringTests(@Autowired val baSakKlient: FamilieBaSakKlient,
+                                   @Autowired val mockserverKlient: MockserverKlient) {
 
     @Test
     fun `Opprett og innvilg en førstegangsbehandling, trigge autobrev`() {
-        val fagsakId = oppretteOgInnvilgeFørstegangsbehandling().data!!.id
+
+        val scenario = mockserverKlient.lagScenario(
+                RestScenario(
+                        søker = RestScenarioPerson(fødselsdato = "1990-04-20", fornavn = "Mor", etternavn = "Søker"),
+                        barna = listOf(RestScenarioPerson(fødselsdato = LocalDate.now().minusYears(6).toString(),
+                                                          fornavn = "Barn1",
+                                                          etternavn = "Barnesen"),
+                                       RestScenarioPerson(fødselsdato = LocalDate.now().minusYears(17).minusMonths(10).toString(),
+                                                          fornavn = "Barn1",
+                                                          etternavn = "Barnesen"))))
+
+        val fagsakId = kjørStegprosessForFGB(tilSteg = StegType.BEHANDLING_AVSLUTTET,
+                                             scenario = scenario,
+                                             familieBaSakKlient = baSakKlient).data!!.id
         baSakKlient.triggerAutobrev18og6år()
 
         await.atMost(80, TimeUnit.SECONDS).withPollInterval(Duration.ofSeconds(1)).until {
@@ -39,77 +50,5 @@ class AutotestBehandlingOmgjøringTests(
         }
 
         assertTrue(baSakKlient.hentFagsak(fagsakId = fagsakId).data?.behandlinger?.filter { it.årsak == BehandlingÅrsak.OMREGNING_6ÅR }?.size == 1)
-    }
-
-    private fun oppretteOgInnvilgeFørstegangsbehandling(): Ressurs<RestFagsak> {
-        val scenario = mockserverKlient!!.lagScenario(
-                RestScenario(
-                        søker = RestScenarioPerson(fødselsdato = "1990-04-20", fornavn = "Mor", etternavn = "Søker"),
-                        barna = listOf(RestScenarioPerson(fødselsdato = LocalDate.now().minusYears(6).toString(),
-                                                          fornavn = "Barn1",
-                                                          etternavn = "Barnesen"),
-                                       RestScenarioPerson(fødselsdato = LocalDate.now().minusYears(17).minusMonths(10).toString(),
-                                                          fornavn = "Barn1",
-                                                          etternavn = "Barnesen"))))
-
-        val søkerId = scenario!!.søker.aktørId!!
-        val barn1 = scenario.barna.first().aktørId!!
-        val barn2 = scenario.barna.last().aktørId!!
-
-
-        baSakKlient.opprettFagsak(søkersIdent = søkerId)
-        val restFagsakMedBehandling = baSakKlient.opprettBehandling(søkersIdent = søkerId)
-        val fagsakId = restFagsakMedBehandling.data!!.id
-
-        val aktivBehandling = Utils.hentAktivBehandling(restFagsak = restFagsakMedBehandling.data!!)
-        val restRegistrerSøknad = RestRegistrerSøknad(søknad = lagSøknadDTO(søkerIdent = morPersonident,
-                                                                            barnasIdenter = listOf(barn1,
-                                                                                                   barn2)),
-                                                      bekreftEndringerViaFrontend = false)
-
-        val restFagsakEtterRegistrertSøknad = baSakKlient.registrererSøknad(
-                behandlingId = aktivBehandling!!.behandlingId,
-                restRegistrerSøknad = restRegistrerSøknad
-        )
-
-        val aktivBehandlingEtterRegistrertSøknad = Utils.hentAktivBehandling(restFagsakEtterRegistrertSøknad.data!!)!!
-        aktivBehandlingEtterRegistrertSøknad.personResultater.forEach { restPersonResultat ->
-            restPersonResultat.vilkårResultater?.filter { it.resultat != Resultat.OPPFYLT }?.forEach {
-                baSakKlient.putVilkår(
-                        behandlingId = aktivBehandlingEtterRegistrertSøknad.behandlingId,
-                        vilkårId = it.id,
-                        restPersonResultat =
-                        RestPersonResultat(personIdent = restPersonResultat.personIdent,
-                                           vilkårResultater = listOf(it.copy(
-                                                   resultat = Resultat.OPPFYLT,
-                                                   periodeFom = LocalDate.now().minusMonths(4)
-                                           ))))
-            }
-        }
-
-        val restFagsakEtterVilkårsvurdering =
-                baSakKlient.validerVilkårsvurdering(
-                        behandlingId = aktivBehandlingEtterRegistrertSøknad.behandlingId
-                )
-
-        val vedtaksperiode = restFagsakEtterVilkårsvurdering.data!!.behandlinger.first().vedtaksperioder.first()
-        baSakKlient.leggTilVedtakBegrunnelse(
-                fagsakId = fagsakId,
-                vedtakBegrunnelse = RestPostVedtakBegrunnelse(
-                        fom = vedtaksperiode.periodeFom!!,
-                        tom = vedtaksperiode.periodeTom,
-                        vedtakBegrunnelse = VedtakBegrunnelseSpesifikasjon.INNVILGET_LOVLIG_OPPHOLD_EØS_BORGER)
-        )
-
-        baSakKlient.sendTilBeslutter(fagsakId = fagsakId)
-        baSakKlient.iverksettVedtak(fagsakId = fagsakId, restBeslutningPåVedtak = RestBeslutningPåVedtak(Beslutning.GODKJENT))
-
-        await.atMost(80, TimeUnit.SECONDS).withPollInterval(Duration.ofSeconds(1)).until {
-            val fagsak = baSakKlient.hentFagsak(fagsakId = fagsakId).data
-            println("FAGSAK: $fagsak")
-            fagsak?.status == FagsakStatus.LØPENDE
-        }
-
-        return baSakKlient.hentFagsak(fagsakId = fagsakId)
     }
 }
--- autotest/src/test/kotlin/no/nav/ba/e2e/commons/DataGenerator.kt
+++ autotest/src/test/kotlin/no/nav/ba/e2e/commons/DataGenerator.kt
@@ -1,18 +1,19 @@
 package no.nav.ba.e2e.commons
 
+import no.nav.ba.e2e.familie_ba_sak.FamilieBaSakKlient
 import no.nav.ba.e2e.familie_ba_sak.domene.*
+import no.nav.ba.e2e.mockserver.domene.RestScenario
+import no.nav.familie.kontrakter.felles.Ressurs
+import org.awaitility.kotlin.await
+import org.awaitility.kotlin.withPollInterval
+import org.junit.jupiter.api.Assertions
+import java.time.Duration
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 val morPersonident = "12345678901"
-val farPersonident = "12345678911"
-val barnPersonident = "01101800033"
-val barn6ÅrPersonident = LocalDate.now().minusYears(6).tilddMMyy() + "00033"
-val barn18ÅrPersonident = LocalDate.now().minusYears(18).tilddMMyy() + "00033"
-val utenlandskBarnPersonident = "01101800044"
-val utenlandskMorPersonident = "00000000005"
-val utenlandskFarPersonident = "00000000006"
 
 fun lagSøknadDTO(søkerIdent: String,
                  barnasIdenter: List<String>): SøknadDTO {
@@ -30,4 +31,82 @@ fun lagSøknadDTO(søkerIdent: String,
     )
 }
 
-fun LocalDate.tilddMMyy() = this.format(DateTimeFormatter.ofPattern("ddMMyy", Locale("nb", "Norway")))
+/**
+ * Dette er en funksjon for å få en førstegangsbehandling til en ønsket tilstand ved test.
+ * Man sender inn steg man ønsker å komme til (tilSteg), scenarioet og BaSakKlienten.
+ */
+fun kjørStegprosessForFGB(
+        tilSteg: StegType,
+        scenario: RestScenario,
+        familieBaSakKlient: FamilieBaSakKlient
+): Ressurs<RestFagsak> {
+    val søkersIdent = scenario.søker.ident!!
+
+    familieBaSakKlient.opprettFagsak(søkersIdent = søkersIdent)
+    val restFagsakMedBehandling = familieBaSakKlient.opprettBehandling(søkersIdent = søkersIdent)
+
+    val aktivBehandling = hentAktivBehandling(restFagsak = restFagsakMedBehandling.data!!)
+    val restRegistrerSøknad = RestRegistrerSøknad(søknad = lagSøknadDTO(søkerIdent = søkersIdent,
+                                                                        barnasIdenter = scenario.barna.map { it.ident!! }),
+                                                  bekreftEndringerViaFrontend = false)
+    val restFagsakEtterRegistrertSøknad =
+            familieBaSakKlient.registrererSøknad(
+                    behandlingId = aktivBehandling!!.behandlingId,
+                    restRegistrerSøknad = restRegistrerSøknad
+            )
+
+    if (tilSteg == StegType.REGISTRERE_PERSONGRUNNLAG || tilSteg == StegType.REGISTRERE_SØKNAD) return restFagsakEtterRegistrertSøknad
+
+    // Godkjenner alle vilkår på førstegangsbehandling.
+    val aktivBehandlingEtterRegistrertSøknad = hentAktivBehandling(restFagsakEtterRegistrertSøknad.data!!)!!
+    aktivBehandlingEtterRegistrertSøknad.personResultater.forEach { restPersonResultat ->
+        restPersonResultat.vilkårResultater?.filter { it.resultat != Resultat.OPPFYLT }?.forEach {
+
+            familieBaSakKlient.putVilkår(
+                    behandlingId = aktivBehandlingEtterRegistrertSøknad.behandlingId,
+                    vilkårId = it.id,
+                    restPersonResultat =
+                    RestPersonResultat(personIdent = restPersonResultat.personIdent,
+                                       vilkårResultater = listOf(it.copy(
+                                               resultat = Resultat.OPPFYLT,
+                                               periodeFom = LocalDate.now().minusMonths(2)
+                                       ))))
+        }
+    }
+
+    val restFagsakEtterVilkårsvurdering =
+            familieBaSakKlient.validerVilkårsvurdering(
+                    behandlingId = aktivBehandlingEtterRegistrertSøknad.behandlingId
+            )
+
+    if (tilSteg == StegType.VILKÅRSVURDERING) return restFagsakEtterVilkårsvurdering
+
+
+    val vedtaksperiode = restFagsakEtterVilkårsvurdering.data!!.behandlinger.first().vedtaksperioder.first()
+    familieBaSakKlient.leggTilVedtakBegrunnelse(
+            fagsakId = restFagsakEtterVilkårsvurdering.data!!.id,
+            vedtakBegrunnelse = RestPostVedtakBegrunnelse(
+                    fom = vedtaksperiode.periodeFom!!,
+                    tom = vedtaksperiode.periodeTom,
+                    vedtakBegrunnelse = VedtakBegrunnelseSpesifikasjon.INNVILGET_LOVLIG_OPPHOLD_EØS_BORGER)
+    )
+
+    val restFagsakEtterSendTilBeslutter =
+            familieBaSakKlient.sendTilBeslutter(fagsakId = restFagsakEtterVilkårsvurdering.data!!.id)
+
+    if (tilSteg == StegType.SEND_TIL_BESLUTTER) return restFagsakEtterSendTilBeslutter
+
+    val restFagsakEtterIverksetting = familieBaSakKlient.iverksettVedtak(fagsakId = restFagsakEtterVilkårsvurdering.data!!.id,
+                                                                         restBeslutningPåVedtak = RestBeslutningPåVedtak(
+                                                                                 Beslutning.GODKJENT))
+    if (tilSteg == StegType.BEHANDLING_AVSLUTTET) {
+        await.atMost(80, TimeUnit.SECONDS).withPollInterval(Duration.ofSeconds(1)).until {
+
+            val fagsak = familieBaSakKlient.hentFagsak(fagsakId = restFagsakEtterIverksetting.data!!.id).data
+            println("FAGSAK: $fagsak")
+            fagsak?.status == FagsakStatus.LØPENDE
+        }
+    }
+
+    return familieBaSakKlient.hentFagsak(fagsakId = restFagsakEtterIverksetting.data!!.id)
+}
