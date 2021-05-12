008eeb4ca4b367fd75e6104470bde6afe2c84623
Nina Kjekstad <ninakjek@users.noreply.github.com>
2021-05-12 13:05:17 +0200

    Gi mer spesifikk feilmelding enn at var FEILET men ønsket SUKSESS ved feilressurs (#68)

--- autotest/src/test/kotlin/no/nav/ba/e2e/commons/Utils.kt
+++ autotest/src/test/kotlin/no/nav/ba/e2e/commons/Utils.kt
@@ -6,6 +6,7 @@ import no.nav.familie.kontrakter.felles.Ressurs
 import org.junit.jupiter.api.Assertions
 import no.nav.ba.e2e.familie_ba_sak.domene.RestVedtak
 import no.nav.ba.e2e.familie_ba_sak.domene.StegType
+import java.lang.IllegalStateException
 import java.time.LocalDate
 
 fun hentAktivBehandling(restFagsak: RestFagsak): RestBehandling? {
@@ -28,11 +29,10 @@ fun hentNåværendeEllerNesteMånedsUtbetaling(behandling: RestBehandling?): Int
 }
 
 fun generellAssertFagsak(restFagsak: Ressurs<RestFagsak>,
-                                 fagsakStatus: FagsakStatus,
-                                 behandlingStegType: StegType? = null,
-                                 behandlingResultat: BehandlingResultat? = null) {
-
-    Assertions.assertEquals(Ressurs.Status.SUKSESS, restFagsak.status)
+                         fagsakStatus: FagsakStatus,
+                         behandlingStegType: StegType? = null,
+                         behandlingResultat: BehandlingResultat? = null) {
+    if (restFagsak.status !== Ressurs.Status.SUKSESS) throw IllegalStateException("generellAssertFagsak feilet. status: ${restFagsak.status.name},  melding: ${restFagsak.melding}")
     Assertions.assertEquals(fagsakStatus, restFagsak.data?.status)
     if (behandlingStegType != null) {
         Assertions.assertEquals(behandlingStegType, hentAktivBehandling(restFagsak = restFagsak.data!!)?.steg)
